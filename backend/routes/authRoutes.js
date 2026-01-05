775a0c3c6830da70493b76665886639b22116b78
Ritesh <riteshkr7578@gmail.com>
2026-01-05 18:54:33 +0530

    auth

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
